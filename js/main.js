f392c433b33cd1c17a707f908588cbcfefa92625
GodDamnedHohil <korec.illia@gmail.com>
2020-09-06 02:26:23 +0300

    added full adaptive main-screen and popup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuCloseBtn = document.querySelector('.menu-close-btn__wrap');
+const navWrap = document.querySelector('.nav__wrap');
+const popupWrap = document.querySelector('.popup__wrap');
+const popupTrigger = document.querySelectorAll('.popup-trigger');
+const popupCloseWrap = document.querySelector('.popup-close__wrap');
+
 function testWebP(callback) {
   var webP = new Image();
   webP.onload = webP.onerror = function () {
@@ -14,3 +21,23 @@ function testWebP(callback) {
   document.querySelector('body').classList.add('no-webp');
   }
 });
+
+menuBtn.addEventListener('click', () => {
+  navWrap.classList.add('active');
+})
+
+menuCloseBtn.addEventListener('click', () => {
+  navWrap.classList.remove('active');
+})
+
+popupTrigger.forEach((item) => {
+  item.addEventListener('click', () => {
+    popupWrap.classList.add('active');
+  })
+})
+
+popupCloseWrap.addEventListener('click', () => {
+  popupWrap.classList.remove('active');
+})
+
+
